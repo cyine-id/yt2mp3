8586c28f1a5d43f0e7c96b22cc69a41a05dcf804
gilang-as <gilangas@outlook.com>
2021-04-28 11:02:57 +0700

    Update Request Header

--- src/server.js
+++ src/server.js
@@ -12,7 +12,18 @@ const MainPage = async (req, res) => {
           `https://ytapivmp3.com/api/button/mp3/${req.query['id']}`,
           {
             headers: {
-              referer: "https://www.easy-youtube-mp3.com/ "
+              accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+              // "accept-encoding": "gzip, deflate, br",
+              "accept-language": "en-US,en;q=0.9",
+              "cache-control": "no-cache",
+              pragma: "no-cache",
+              referer: "https://www.easy-youtube-mp3.com/",
+              "sec-fetch-dest": "iframe",
+              "sec-fetch-mode": "navigate",
+              "sec-fetch-site": "cross-site",
+              "sec-gpc": 1,
+              "upgrade-insecure-requests": 1,
+              "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36"
             }
           }
         );
